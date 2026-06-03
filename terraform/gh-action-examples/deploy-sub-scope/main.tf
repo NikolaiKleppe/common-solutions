3ed2dc75a51a586a680ccecbd462f018d69c0731
Nikolai <nikolai__92@hotmail.com>
2026-06-03 18:48:50 +0200

    update

--- terraform/gh-action-examples/deploy-sub-scope/main.tf
+++ terraform/gh-action-examples/deploy-sub-scope/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "rg-example-deploy-sub-scope"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-example-deploy-sub-scope-2"
+  location = "West Europe"
+}
