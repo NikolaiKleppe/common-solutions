759ca7e7511ffae4506f3c2e72acb7bce8ffd980
Nikolai <nikolai__92@hotmail.com>
2026-06-03 16:58:23 +0200

    update

---
+++ terraform/gh-action-examples/deploy-mg-scope/main.tf
@@ -0,0 +1,18 @@
+variable "management_group_id" {
+	description = "Management group id"
+	type        = string
+}
+
+resource "azurerm_management_group_policy_assignment" "apim_secret_named_values_audit" {
+	name                 = "gh-action-apim-secret-named-values-kv-audit"
+	display_name         = "gh-action-examples-API Management secret named values should be stored in Azure Key Vault"
+	description          = "Example assignment of a built-in policy definition at management group scope in Audit mode."
+	management_group_id  = "/providers/Microsoft.Management/managementGroups/${var.management_group_id}"
+	policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/f1cc7827-022c-473e-836e-5a51cae0b249"
+
+	parameters = jsonencode({
+		effect = {
+			value = "Audit"
+		}
+	})
+}
---
+++ terraform/gh-action-examples/deploy-mg-scope/terraform.tfvars
@@ -0,0 +1,1 @@
+management_group_id = "0cf2e078-fc79-4ccd-9b6b-1fc6e11bad68" # Core Services
